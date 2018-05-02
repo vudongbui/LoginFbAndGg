e2bbea8ba266695eb410f87409aab282257e6e5a
vudongbui <vudongbui@gmail.com>
2018-05-03 01:45:22 +0700

    homework

--- app/src/main/java/com/example/owner/login/MainActivity.java
+++ app/src/main/java/com/example/owner/login/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.owner.login;
 
+import android.app.ProgressDialog;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
@@ -10,6 +11,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -18,9 +21,12 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -30,13 +36,75 @@ public class MainActivity extends AppCompatActivity {
     private static final String EMAIL = "email";
     private static final String USER_POSTS = "user_posts";
 
+    CallbackManager callbackManager;
+    LoginButton mLoginButton;
+    TextView tvName;
+    ImageView ivAvatar;
+    ProgressDialog mDialog;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode,resultCode,data);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LoginButton mLoginButton = findViewById(R.id.login_button);
+
+        callbackManager = CallbackManager.Factory.create();
+        mLoginButton = findViewById(R.id.login_button);
+        tvName = findViewById(R.id.tv_fblogin);
+        ivAvatar = findViewById(R.id.iv_fb);
+
+        mLoginButton.setReadPermissions(Arrays.asList("public_profile"));
+        mLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                mDialog = new ProgressDialog(MainActivity.this);
+                mDialog.setMessage("loading...");
+                mDialog.show();
+
+                String accesstoken = loginResult.getAccessToken().getToken();
+
+                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        mDialog.dismiss();
+                        try {
+                            String nameFb = object.getString("name");
+                            tvName.setText(nameFb);
+                            String idFb = object.getString("id");
+                            Picasso.get().load("https://graph.facebook.com/" + idFb + "/picture?type=large")
+                                    .resize(100,100)
+                                    .centerCrop()
+                                    .into(ivAvatar);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                });
+
+//                Bundle parameters = new Bundle();
+//                parameters.putString();
+//                graphRequest.setParameters(parameters);
+                graphRequest.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
 
         try {
             PackageInfo info = getPackageManager().getPackageInfo(
@@ -54,5 +122,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+//    private void getInfor(){
+//        try{
+//            URL Avatar = new URL("https://graph.facebook.com/"+obj)
+//        }
+//    }
 
 }
